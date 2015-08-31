dbc5161bca80fc2c80900b9d6d249c6bf29256c6
178inaba <magic.brain.evolution.7.0@gmail.com>
2015-08-31 17:42:36 +0900

    add loading text

--- js/index.js
+++ js/index.js
@@ -1,32 +1,63 @@
 var progress = {
 	bar: $("#test-progress"),
 	sr: $("#test-progress span.sr-only"),
+	startBtn: $("#btn-start"),
+	stopBtn: $("#btn-stop"),
 	clearBtn: $("#btn-clear"),
-	intervalID: null,
+	loadingP: $("#loading"),
+	moveID: null,
+	loadingID: null,
+	loadingDot: 0,
 	move: function(moveVal) {
 		console.debug("progress.move");
 
-		now = Number($("#test-progress").attr("aria-valuenow"));
-		now += moveVal;
+		nowVal = Number($("#test-progress").attr("aria-valuenow"));
+		nowVal += moveVal;
 
-		this.bar.css("width", now + "%");
-		this.bar.attr("aria-valuenow", now);
-		this.sr.text(now + "% Complete");
+		this.bar.css("width", nowVal + "%");
+		this.bar.attr("aria-valuenow", nowVal);
+		this.sr.text(nowVal + "% Complete");
 
-		if (now === 100) {
+		if (nowVal === 100) {
 			this.stop();
+		}
+	},
+	loading: function() {
+		console.debug("progress.loading");
+
+		var dot = "";
+		for (var i = 0; i < this.loadingDot; i++) {
+			dot += ".";
+		}
+
+		this.loadingP.text("now loading" + dot);
+
+		this.loadingDot++;
+		if (this.loadingDot === 4) {
+			this.loadingDot = 0;
 		}
 	},
 	start: function() {
 		console.debug("progress.start");
 
+		// button
+		this.startBtn.attr("disabled", "disabled");
 		this.clearBtn.attr("disabled", "disabled");
-		this.intervalID = setInterval("progress.move(1)", 1000);
+		this.stopBtn.removeAttr("disabled");
+
+		this.loadingID = setInterval("progress.loading()", 200);
+		this.moveID = setInterval("progress.move(1)", 200);
 	},
 	stop: function() {
 		console.debug("progress.stop");
 
-		clearInterval(this.intervalID);
+		clearInterval(this.loadingID);
+		clearInterval(this.moveID);
+		this.loadingP.text("stop");
+
+		// button
+		this.stopBtn.attr("disabled", "disabled");
+		this.startBtn.removeAttr("disabled");
 		this.clearBtn.removeAttr("disabled");
 	},
 	clear: function() {
@@ -35,5 +66,6 @@ var progress = {
 		this.bar.css("width", "0%");
 		this.bar.attr("aria-valuenow", 0);
 		this.sr.text("0% Complete");
+		this.loadingP.text("");
 	}
 };
